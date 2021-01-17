3c44c9cc49c405327c750d93587cc5fb9ac0f848
emekamykael45 <emekamykael45@gmail.com>
2021-01-17 22:23:15 +0100

    initial commit and app architecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Account from "./Account";
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = () => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Display Active Users Account Details</h1>
+      {error && <p>{error.message}</p>}
+      {!isLoading ? (
+        users.map((user) => <Account key={user.username} user={user} />)
+      ) : (
+        <h3>Fetching users...</h3>
+      )}
+    </React.Fragment>
   );
-}
+};
 
 export default App;
